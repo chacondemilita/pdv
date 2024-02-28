5d514c7e4f2cd19dbb266b734972b1b4ed4cfdf3
alejandro <ale.ch@cloudsultor.com>
2024-02-28 15:20:55 -0300

    nuevas modificaciones

--- ventas-borrador/ventas.py
+++ ventas-borrador/ventas.py
@@ -9,19 +9,19 @@ from kivy.uix.recycleview.layout import LayoutSelectionBehavior
 from kivy.uix.popup import Popup
 
 inventario=[
-	{'codigo': '111', 'nombre': 'leche 1L', 'precio': 20.0, 'cantidad': 20},
-	{'codigo': '222', 'nombre': 'cereal 500g', 'precio': 50.5, 'cantidad': 15}, 
-	{'codigo': '333', 'nombre': 'yogurt 1L', 'precio': 25.0, 'cantidad': 10},
-	{'codigo': '444', 'nombre': 'helado 2L', 'precio': 80.0, 'cantidad': 20},
-	{'codigo': '555', 'nombre': 'alimento para perro 20kg', 'precio': 750.0, 'cantidad': 5},
-	{'codigo': '666', 'nombre': 'shampoo', 'precio': 100.0, 'cantidad': 25},
-	{'codigo': '777', 'nombre': 'papel higiénico 4 rollos', 'precio': 35.5, 'cantidad': 30},
-	{'codigo': '888', 'nombre': 'jabón para trastes', 'precio': 65.0, 'cantidad': 5},
-	{'codigo': '999', 'nombre': 'refresco 600ml', 'precio': 15.0, 'cantidad': 10}
+    {'codigo': '111', 'nombre': 'leche 1L', 'precio': 20.0, 'cantidad': 20},
+    {'codigo': '222', 'nombre': 'cereal 500g', 'precio': 50.5, 'cantidad': 15}, 
+    {'codigo': '333', 'nombre': 'yogurt 1L', 'precio': 25.0, 'cantidad': 10},
+    {'codigo': '444', 'nombre': 'helado 2L', 'precio': 80.0, 'cantidad': 20},
+    {'codigo': '555', 'nombre': 'alimento para perro 20kg', 'precio': 750.0, 'cantidad': 5},
+    {'codigo': '666', 'nombre': 'shampoo', 'precio': 100.0, 'cantidad': 25},
+    {'codigo': '777', 'nombre': 'papel higiénico 4 rollos', 'precio': 35.5, 'cantidad': 30},
+    {'codigo': '888', 'nombre': 'jabón para trastes', 'precio': 65.0, 'cantidad': 5},
+    {'codigo': '999', 'nombre': 'refresco 600ml', 'precio': 15.0, 'cantidad': 10}
 ]
 
 class SelectableRecycleBoxLayout(FocusBehavior, LayoutSelectionBehavior,
-                                 RecycleBoxLayout):
+                                RecycleBoxLayout):
     ''' Adds selection and focus behaviour to the view. '''
     touch_deselect_last = BooleanProperty(True)
 
@@ -119,87 +119,87 @@ class RV(RecycleView):
 
 
 class ProductoPorNombrePopup(Popup):
-	def __init__(self, input_nombre, agregar_producto_callback, **kwargs):
-		super(ProductoPorNombrePopup, self).__init__(**kwargs)
-		self.input_nombre=input_nombre
-		self.agregar_producto=agregar_producto_callback
+    def __init__(self, input_nombre, agregar_producto_callback, **kwargs):
+        super(ProductoPorNombrePopup, self).__init__(**kwargs)
+        self.input_nombre=input_nombre
+        self.agregar_producto=agregar_producto_callback
 
-	def mostrar_articulos(self):
-		self.open()
-		for nombre in inventario:
-			if nombre['nombre'].lower().find(self.input_nombre)>=0:
-				producto={'codigo': nombre['codigo'], 'nombre': nombre['nombre'], 'precio': nombre['precio'], 'cantidad': nombre['cantidad']}
-				self.ids.rvs.agregar_articulo(producto)
+    def mostrar_articulos(self):
+        self.open()
+        for nombre in inventario:
+            if nombre['nombre'].lower().find(self.input_nombre)>=0:
+                producto={'codigo': nombre['codigo'], 'nombre': nombre['nombre'], 'precio': nombre['precio'], 'cantidad': nombre['cantidad']}
+                self.ids.rvs.agregar_articulo(producto)
 
-	def seleccionar_articulo(self):
-		indice=self.ids.rvs.articulo_seleccionado()
-		if indice>=0:
-			_articulo=self.ids.rvs.data[indice]
-			articulo={}
-			articulo['codigo']=_articulo['codigo']
-			articulo['nombre']=_articulo['nombre']
-			articulo['precio']=_articulo['precio']
-			articulo['cantidad_carrito']=1
-			articulo['cantidad_inventario']=_articulo['cantidad']
-			articulo['precio_total']=_articulo['precio']
-			if callable(self.agregar_producto):
-				self.agregar_producto(articulo)
-			self.dismiss()
+    def seleccionar_articulo(self):
+        indice=self.ids.rvs.articulo_seleccionado()
+        if indice>=0:
+            _articulo=self.ids.rvs.data[indice]
+            articulo={}
+            articulo['codigo']=_articulo['codigo']
+            articulo['nombre']=_articulo['nombre']
+            articulo['precio']=_articulo['precio']
+            articulo['cantidad_carrito']=1
+            articulo['cantidad_inventario']=_articulo['cantidad']
+            articulo['precio_total']=_articulo['precio']
+            if callable(self.agregar_producto):
+                self.agregar_producto(articulo)
+            self.dismiss()
 
 
 
 class VentasWindow(BoxLayout):
-	def __init__(self, **kwargs):
-		super().__init__(**kwargs)
-		self.total=0.0
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.total=0.0
 
-	def agregar_producto_codigo(self, codigo):
-		for producto in inventario:
-			if codigo==producto['codigo']:
-				articulo={}
-				articulo['codigo']=producto['codigo']
-				articulo['nombre']=producto['nombre']
-				articulo['precio']=producto['precio']
-				articulo['cantidad_carrito']=1
-				articulo['cantidad_inventario']=producto['cantidad']
-				articulo['precio_total']=producto['precio']
-				self.agregar_producto(articulo)
-				self.ids.buscar_codigo.text=''
-				break
+    def agregar_producto_codigo(self, codigo):
+        for producto in inventario:
+            if codigo==producto['codigo']:
+                articulo={}
+                articulo['codigo']=producto['codigo']
+                articulo['nombre']=producto['nombre']
+                articulo['precio']=producto['precio']
+                articulo['cantidad_carrito']=1
+                articulo['cantidad_inventario']=producto['cantidad']
+                articulo['precio_total']=producto['precio']
+                self.agregar_producto(articulo)
+                self.ids.buscar_codigo.text=''
+                break
 
-	def agregar_producto_nombre(self, nombre):
-		self.ids.buscar_nombre.text=''
-		popup=ProductoPorNombrePopup(nombre, self.agregar_producto)
-		popup.mostrar_articulos()
+    def agregar_producto_nombre(self, nombre):
+        self.ids.buscar_nombre.text=''
+        popup=ProductoPorNombrePopup(nombre, self.agregar_producto)
+        popup.mostrar_articulos()
 
-	def agregar_producto(self, articulo):
-		self.total+=articulo['precio']
-		self.ids.sub_total.text= '$ '+"{:.2f}".format(self.total)
-		self.ids.rvs.agregar_articulo(articulo)
+    def agregar_producto(self, articulo):
+        self.total+=articulo['precio']
+        self.ids.sub_total.text= '$ '+"{:.2f}".format(self.total)
+        self.ids.rvs.agregar_articulo(articulo)
 
 
-	def eliminar_producto(self):
-		print("eliminar_producto presionado")
+    def eliminar_producto(self):
+        print("eliminar_producto presionado")
 
-	def modificar_producto(self):
-		print("eliminar_producto presionado")
+    def modificar_producto(self):
+        print("eliminar_producto presionado")
 
-	def pagar(self):
-		print("pagar")
+    def pagar(self):
+        print("pagar")
 
-	def nueva_compra(self):
-		print("nueva_compra")
+    def nueva_compra(self):
+        print("nueva_compra")
 
-	def admin(self):
-		print("Admin presionado")
+    def admin(self):
+        print("Admin presionado")
 
-	def signout(self):
-		print("Signout presionado")
+    def signout(self):
+        print("Signout presionado")
 
 class VentasApp(App):
-	def build(self):
-		return VentasWindow()
+    def build(self):
+        return VentasWindow()
 
 
 if __name__=='__main__':
-	VentasApp().run()
+    VentasApp().run()
